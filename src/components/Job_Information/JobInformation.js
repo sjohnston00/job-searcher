1fcfda628a1aa0308ccea8943ad5a5dc64d7f346
Sam Johnston <johnstonsam712@gmail.com>
2020-11-24 14:25:54 +0000

    Job Information page complete

--- src/components/Job_Information/JobInformation.js
+++ src/components/Job_Information/JobInformation.js
@@ -5,21 +5,43 @@ import styles from './JobInformation.module.css'
 
 export default function JobInformation() {
   //declaring state variables
-
-
-  
-  //consider currentJob for state instead of SOC so we can then use this state for populate the description section of the job
   const [currentJob, setCurrentJob] = useState({});
   const [errorMessage, seterrorMessage] = useState('');
   const [estimatePay, setestimatePay] = useState({});
+  const [estimateHours, setEstimateHours] = useState(0);
+  const [relatedCourses, setRelatedCourses] = useState([]);
+  const [unemploymentRate, setUnemploymentRate] = useState({});
+  const [averageWage, setAverageWage] = useState(0);
   const [searched, setSearched] = useState(false);
   const jobInput = useRef(null);
+  const regionDropdown = useRef(null);
 
 
+  //once the currentJob has changed search for estimate pay
   useEffect(() => {
     const chart = async () => {
-      const req = await axios.get(`https://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=${currentJob.soc}`);
+      let url = `https://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=${currentJob.soc}`;
+      if (regionDropdown.current.value > 0) {
+        url = `https://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=${currentJob.soc}&filters=region%3A${regionDropdown.current.value}`;
+      }
+
+      const req = await axios.get(url);
       const data = await req.data;
+
+      if (!data.series) {
+        setestimatePay({
+          labels: ['Not Enough Data'],
+          datasets: [
+            {
+              label: 'Not Enough Data',
+              data: [],
+              backgroundColor: ['rgba(64, 59, 141, 0.7)'],
+              borderWidth: 4,
+            }
+          ]
+        })
+        return
+      }
 
       data.series.sort((a,b) => {
         if (a.year < b.year) {
@@ -42,8 +64,6 @@ export default function JobInformation() {
         return obj.estpay
       })
 
-
-
       setestimatePay({
         labels: [...years],
         datasets: [
@@ -57,7 +77,129 @@ export default function JobInformation() {
       })
     }
     chart()
-  }, [currentJob]) //run of start of this component redering
+  }, [currentJob, regionDropdown]) 
+
+  //once the current job state changes get Unemployment History
+  useEffect(() => {
+    const UnemploymentHistory = async () => {
+
+      let url = `https://api.lmiforall.org.uk/api/v1/lfs/unemployment?soc=${currentJob.soc}`;
+      if (regionDropdown.current.value > 0) {
+        url = `https://api.lmiforall.org.uk/api/v1/lfs/unemployment?soc=${currentJob.soc}&filterBy=region%3A${regionDropdown.current.value}`;
+      }
+      //use the SOC in state to get the average unemployment rates from the LMI For ALL API
+      const req = await axios.get(url)
+      const data = await req.data;
+
+      if (!data.years) {
+        setUnemploymentRate({
+          labels: ['Not Enough Data'],
+          datasets: [
+            {
+              label: 'Not Enough Data',
+              data: [],
+              backgroundColor: ['rgba(64, 59, 141, 0.7)'],
+              borderWidth: 4,
+            }
+          ]
+        })
+        return
+      }
+  
+      data.years.sort((a,b) => {
+        if (a.year < b.year) {
+          return -1
+        }
+        if (a.year > b.year) {
+          return 1
+        }
+        return 0;
+      });
+  
+      const years = data.years.map(obj => {
+        return obj.year
+      })
+      const unemploymentRate = data.years.map(obj => {
+        return obj.unemprate
+      })
+  
+      setUnemploymentRate({
+        labels: [...years],
+        datasets: [
+          {
+            label: 'Rate',
+            data: [...unemploymentRate],
+            backgroundColor: ['rgba(64, 59, 141, 0.7)'],
+            borderWidth: 4,
+          }
+        ]
+      })
+    };
+    UnemploymentHistory();
+  }, [currentJob, regionDropdown]);
+
+  //once the current job state has changed then searched for the working hours
+  useEffect(() => {
+    const getWorkingHours = async () => {
+
+      let url = `https://api.lmiforall.org.uk/api/v1/ashe/estimateHours?soc=${currentJob.soc}`;
+      if (regionDropdown.current.value > 0) {
+        url = `https://api.lmiforall.org.uk/api/v1/ashe/estimateHours?soc=${currentJob.soc}&filters=region%3A${regionDropdown.current.value}`;
+      }
+      
+      const workingHoursReq = await axios.get(url);
+      const workingHours = await workingHoursReq.data;
+  
+      if (!workingHours.series) {
+        setEstimateHours(0);
+        return;
+      }
+      let sum = 0;
+      workingHours.series.forEach(element => {
+        sum += parseInt(element.hours, 10);
+      });
+      setEstimateHours(Math.round(sum/workingHours.series.length));
+    };
+    getWorkingHours();
+  }, [currentJob, regionDropdown]);
+
+
+  //once the current job state has changed then search for the related course
+  useEffect(() => {
+    try {
+      const getRelatedCourse = async () => {
+      const req = await axios.get(`https://api.lmiforall.org.uk/api/v1/hesa/courses/${currentJob.soc}`);
+      const data = await req.data;
+    
+      if (!data.years || data.years.length === 0) {
+        setRelatedCourses([{name: 'No related courses to this job'}])
+        return 
+      }
+    
+      const relatedCoursesArray = data.years[0].courses.filter(element => {
+        return element.percentage > 50; 
+      });
+      setRelatedCourses(relatedCoursesArray);
+      };
+      getRelatedCourse();
+    } catch (error) {
+      setRelatedCourses([]);      
+    }
+  }, [currentJob]);
+
+  //once the current job state has changed then set the average salary
+  useEffect(() => {
+    try {
+      let sum = 0;
+      estimatePay.datasets[0].data.forEach(element => {
+        sum += parseInt(element, 10);
+      });
+      setAverageWage(Math.round(sum/estimatePay.datasets[0].data.length));
+    } catch (error) {
+      setAverageWage(0);      
+    }
+  }, [estimatePay, regionDropdown]);
+
 
 
   const searchJob = async () => {
@@ -68,48 +210,39 @@ export default function JobInformation() {
 
     const job = await axios.get(`https://api.lmiforall.org.uk/api/v1/soc/search?q=${jobInput.current.value}`);
     if (job.data.length === 0) {
-      seterrorMessage('Not Results Found');
+      seterrorMessage('No Results Found');
       return;
     }
     seterrorMessage('');
     const jobdata = await job.data[0];
-    setCurrentJob(jobdata);
+    await setCurrentJob(jobdata);
     setSearched(true)
   };
-
-  // const employmentHistory = async () => {
-  //   //use the SOC in state to get the average unemployment rates from the LMI For ALL API
-  // };
-
-  // const getWorkingHours = async () => {
-  //   //use the SOC in state to get the average working hours from the LMI For ALL API
-  //   //then set the data to the chart
-  // };
-
-  // const getRelatedCourse = async () => {
-  //   //use the SOC in state to get the related courses from the LMI For ALL API
-  // }
-
-  // const averageYearWage = (weeklywage) => {
-  //   //this will be an array of the weekly salaries for each year the job has
-  //   //e.g SOC 2136 
-  //   // 2013 = estpay = £534
-  //   // 2015 = estpay = £235
-  //   // 2018 = estpay = £611
-
-  //   //calculate the yearly wage for each year then add them up and divide them by the number in the array
-  //   //return average
-  // };
+  
 
   return (
     <>
-      <div>
-        Job Information
-      </div>
-
+      <h1>Job Information</h1>
       <div>
         <input type='text' ref={jobInput} placeholder='Search for a job...'/>
         <button onClick={searchJob}>Search</button>
+        <br/>
+        <br/>
+        <select id='Region-Filter' ref={regionDropdown} onChange={searchJob}>
+          <option value='0'>All Areas</option>
+          <option value='1'>London</option>
+          <option value='2'>South East (England)</option>
+          <option value='3'>East of England</option>
+          <option value='4'>South West (England)</option>
+          <option value='5'>West Midlands (England)</option>
+          <option value='6'>East Midlands (England)</option>
+          <option value='7'>Yorkshire and the Humber</option>
+          <option value='8'>North West (England)</option>
+          <option value='9'>North East (England)</option>
+          <option value='10'>Wales</option>
+          <option value='11'>Scotland</option>
+          <option value='12'>Northern Ireland</option>
+        </select>
         {errorMessage.length > 0 && <p>{errorMessage}</p>}
       </div>
       {!searched ? 
@@ -129,6 +262,10 @@ export default function JobInformation() {
             <p>Tasks: {currentJob.tasks}</p>
             <br/>
             <p>SOC: {currentJob.soc}</p>
+            <br/>
+            <p>Estimate Hours: {estimateHours ? estimateHours : 'Not Enough Data'}</p>
+            <br/>
+            <p>Average Salary: £{averageWage}</p>
             <br/>
           </div>  
           <div className={styles.chart_container}>
@@ -151,6 +288,35 @@ export default function JobInformation() {
           </div>
         </div>
         <p>Qualifications: {currentJob.qualifications}</p>
+        <br/>
+        <h3>Related Courses</h3>
+        <ul>
+          {relatedCourses.map((course, i) => {
+            return (
+              <li key={i}>{course.name}</li>
+            )
+          })}
+        </ul>
+
+        <h2>Unemployment History</h2>
+        <div className={styles.chart_container}>
+            <Line data={unemploymentRate} options={{
+              responsive: true,
+              tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+                        if (label) {
+                            label += ': ';
+                        }
+                        label += `${Math.round(tooltipItem.yLabel * 100) / 100}%`;
+                        return label;
+                    }
+                }
+              }
+            }}/>
+          </div>
       </>
       }
       
