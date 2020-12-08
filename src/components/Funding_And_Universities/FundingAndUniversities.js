4fc15e95effc8fb84fac6adde1b74da632ba75f1
Sam Johnston <johnstonsam712@gmail.com>
2020-12-08 12:09:43 +0000

    Adding local universities file

--- src/components/Funding_And_Universities/FundingAndUniversities.js
+++ src/components/Funding_And_Universities/FundingAndUniversities.js
@@ -1,38 +1,61 @@
 import React, { useState, useEffect} from 'react'
-import axios from 'axios';
 import styles from './FundingAndUniversities.module.css';
 
 export default function FundingAndUniversities() {
 
   const [searchBox, setSearchbox] = useState('');
   const [universities, setUniverities] = useState([]);
+  const [universitiesData, setUniversitiesData] = useState([]);
+
 
   useEffect(() => {
-
-    const search = async () => {
-      try {
-        const req = await axios.get(`http://universities.hipolabs.com/search?name=${searchBox}&country=United%20Kingdom`);
-        const data = await req.data;
-  
-        const filteredData = data.filter((obj, index, self) => {
+    fetch('http://universities.hipolabs.com/search?country=United%20Kingdom')
+    .then(res => res.json())
+    //filter the array to remove duplicates
+    .then(json => { 
+      setUniversitiesData(json.filter((obj, index, self) => {
+        return self.findIndex(t => t.name === obj.name) === index;
+      }));
+      setUniverities(json.filter((obj, index, self) => {
+        return self.findIndex(t => t.name === obj.name) === index;
+      }));
+    })
+    .catch(() => {
+      fetch('UK_Universities.json')
+      .then(res => res.json())
+      //filter the array to remove duplicates
+      .then(json => {
+        setUniversitiesData(json.filter((obj, index, self) => {
           return self.findIndex(t => t.name === obj.name) === index;
-        });
-  
-        setUniverities(filteredData);
-      } catch (error) {
-        alert('This page does not work as the API where the data comes from is a HTTP endpoint. For safety and security reason I have disabled this functionality.');      
-      }
+        }));
+        setUniverities(json.filter((obj, index, self) => {
+          return self.findIndex(t => t.name === obj.name) === index;
+        }));
+      })
+    })
+
+  }, [])
+
+  const filteredData = (e) => {
+    setSearchbox(e.target.value);
+    
+    if (e.target.value === null || e.target.value === '') {
+      setUniverities(universitiesData);
+      return
     }
 
-    search();
-  }, [searchBox])
+    const NewfilteredData = universitiesData.filter( obj => {
+      return obj.name.toLowerCase().includes(searchBox.toLowerCase());
+    });
+    setUniverities(NewfilteredData)
+  }
   return (
     <>
       <div className={styles.container}>
         <h1 className={styles.heading}>Universities and Funding</h1>
 
         <div className={styles.input_group}>
-          <input type='text' value={searchBox} onChange={(e) => setSearchbox(e.target.value)} placeholder='Search for a university...'></input>
+          <input type='text' value={searchBox} onChange={filteredData} placeholder='Search for a university...'></input>
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"/>
             </svg>
@@ -42,10 +65,10 @@ export default function FundingAndUniversities() {
 
           {universities.length === 0 ? <span>No Results</span> : <span>Results - {universities.length}</span>}
           
-          {universities.map((uni) => {
+          {universities.map((uni, id) => {
             const webPage = uni.web_pages[0]; 
             return(
-              <a href={webPage} rel="noreferrer" target="_blank" className={styles.uni}>
+              <a href={webPage} key={id} rel="noreferrer" target="_blank" className={styles.uni}>
                 <div className={styles.left}>
                   <p>{uni.name}</p>
                   <p>{webPage}</p>
